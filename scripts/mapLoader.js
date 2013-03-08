dc047b8ca5c610496af90e6db58997ad6d3cdadd
Andrey Kokorev <a.d.kokorev@yandex.ru>
2013-03-08 14:33:33 +0400

    Изменил персонажа, добавил ему анимацию

--- scripts/mapLoader.js
+++ scripts/mapLoader.js
@@ -1,4 +1,26 @@
-function Map(src){
-	
+function xhrGet(reqUri,callback) {
+	var xhr = new XMLHttpRequest();
 
+	xhr.open("GET", reqUri, true);
+	xhr.onload = callback;
+
+	xhr.send();
+}
+
+function Map(mapName){
+	this.sprites = [];
+	this.loaded = false;
+	var mapJSON;
+	function load(){
+		xhrGet("images/" + mapName + ".json", function(data){console.log(data)});
+		console.log(mapJSON);
+	}
+	function parseJSON(data){
+		// mapJSON = 
+		console.log(data);
+		console.log(data.responseText);
+		console.log(JSON.parse('{"result":true,"count":1}'));
+	}
+
+	load();
 }
